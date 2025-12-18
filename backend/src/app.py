7642e3b0bda943afb7d9f13bcace25e7cbd850c1
Bello-Pluto Salvatore <info@mpalondon.co.uk>
2025-12-18 20:40:13 +0000

    updated favicon

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routes import challenge, webhooks
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 app = FastAPI()
 
 app.add_middleware(CORSMiddleware,
-                   allow_origins=["http://localhost:5173","http://localhost:5174"],
+                   allow_origins=[os.getenv("FRONTEND_URL")],
                    allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"])
