f8f37533f85a2ad91e370ae15954bd81494d0288
Bello-Pluto Salvatore <info@mpalondon.co.uk>
2025-12-18 23:23:40 +0000

    issue with render. testing by forcing URL

--- frontend/src/auth/ClerkProviderWithRoutes.jsx
+++ frontend/src/auth/ClerkProviderWithRoutes.jsx
@@ -2,6 +2,7 @@ import {ClerkProvider} from "@clerk/clerk-react";
 import {BrowserRouter} from "react-router-dom";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Publishable Key')
@@ -9,7 +10,12 @@ if (!PUBLISHABLE_KEY) {
 
 export default function ClerkProviderWithRoutes({children}){
     return(
-        <ClerkProvider publishableKey = {PUBLISHABLE_KEY}>
+        <ClerkProvider
+            publishableKey = {PUBLISHABLE_KEY}
+            afterSignInUrl={FRONTEND_URL}
+            afterSignUpUrl={FRONTEND_URL}
+            afterSignOutUrl={`${FRONTEND_URL}/sign-in`}
+            navigate={(to) => (window.location.href = to)}>
             <BrowserRouter>{children}</BrowserRouter>
         </ClerkProvider>
     )
