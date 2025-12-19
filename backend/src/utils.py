8e0801d8e70ea96eaa0a7561151f189627055e76
Bello-Pluto Salvatore <info@mpalondon.co.uk>
2025-12-19 19:16:59 +0000

    fixed an authorised parties url

--- backend/src/utils.py
+++ backend/src/utils.py
@@ -12,7 +12,7 @@ def authenticate_and_get_user_details(request):
         request_state = clerk_sdk.authenticate_request(
             request,
             AuthenticateRequestOptions(
-                authorized_parties=["http://localhost:5173","http://localhost:5174"],
+                authorized_parties=["http://localhost:5173","http://localhost:5174",os.getenv("FRONTEND_URL")],
                 jwt_key=os.getenv("JWT_KEY")
             )
         )
