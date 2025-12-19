80f2bdbf44be4f14711f927771bed8f843523060
Bello-Pluto Salvatore <info@mpalondon.co.uk>
2025-12-19 20:57:14 +0000

    use effect to reset selectedoption and shouldshowexplanation

--- frontend/src/challenge/ChallengeGenerator.jsx
+++ frontend/src/challenge/ChallengeGenerator.jsx
@@ -48,8 +48,8 @@ export default function ChallengeGenerator(){
     }
 
     const getNextResetTime = () => {
-        if (!quota?.last_reset_data) return null
-        const resetDate = new Date(quota.last_reset_data)
+        if (!quota?.last_reset_date) return null
+        const resetDate = new Date(quota.last_reset_date)
         resetDate.setHours(resetDate.getHours()+24)
         return resetDate
     }
